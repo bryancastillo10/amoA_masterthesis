107a33efe50b069d7e261aae721f9b41dd2d7db0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2022-05-17 20:34:34 +0800

    Update Ordinationplot.R

--- Ordinationplot.R
+++ Ordinationplot.R
@@ -20,6 +20,7 @@ spe.hell<-decostand(log1p(spe.mat),method="hellinger")
 #arguments in the factor() function
 envwater$Site<-factor(envwater$Site)
 Site<-envwater[,2]
+envi<- envwater[,c(3:11)]
 
 #PCA for species-site interaction: unconstrained analysis
 dist.spe<-vegdist(spe.hell, method="euclidean")
@@ -28,7 +29,6 @@ df_site<-data.frame(Site=rownames(mod$centroids),data.frame(mod$centroids))
 
 
 #RDA for species-envi interaction: constrained analysis
-#RDA
 rdamodel<-rda(spe.hell~.,envi)
 df_species  <- data.frame(summary(rdamodel)$species[,1:2])
 df_environ  <- scores(rdamodel, display = "bp") 
@@ -37,7 +37,7 @@ df_environ  <- scores(rdamodel, display = "bp")
 #RDA scores and biplot point coordinates
 df_species  <- data.frame(summary(rdamodel)$species[,1:2])
 df_environ  <- scores(rdamodel, display = 'bp') 
-df_site <- scores(rdamodel,display='cn')
+
 
 #Returns the value of percentage of RDA scores
 RDA1_varex<-round(summary(rdamodel)$cont$importance[2,1]*100,2)
@@ -51,60 +51,60 @@ ggplot(df_species,aes(x=RDA1, y=RDA2))+
   geom_hline(yintercept=0,linetype="dashed")+
   geom_vline(xintercept=0,linetype="dashed")+
   coord_fixed()+
- 
-   #Plot Environmental Parameters
+  
+  #Plot Environmental Parameters
   # In the following parts, you can observe that I manually set up the
   #geom functions so that I can adjust their color,size,and shape,
   #per individual coordinates, so the settings would depend on your preferences
   #Check df_environ in the console to manually know the dataframe index/position of the coordinates
-  geom_segment(data=df_environ[4,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[1,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="BOD")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
-  geom_segment(data=df_environ[5,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[2,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="COD"),  
                size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
-  geom_segment(data=df_environ[6,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[3,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="NH3")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+  
-  geom_segment(data=df_environ[7,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[4,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="NO3")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
-  geom_segment(data=df_environ[8,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[5,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="PO4")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
-  geom_segment(data=df_environ[9,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[6,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="Cl")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
-  geom_segment(data=df_environ[10,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[7,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="pH")  
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
-  geom_segment(data=df_environ[11,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[8,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="Temperature")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+  
-  geom_segment(data=df_environ[12,1:2,drop=FALSE], 
+  geom_segment(data=df_environ[9,1:2,drop=FALSE], 
                aes(x=0, xend=RDA1*scaling_factor,y=0, 
                    yend=RDA2*scaling_factor,color="DO")
                ,size=0.8,arrow=arrow(length=unit(0.02,"npc")))+
   #Sites    
-  geom_point(data=df_site[1,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray24",size=1.5)+
-  geom_text(data=df_site[1,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray25",
+  geom_point(data=df_site[1,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray24",size=1.5)+
+  geom_text(data=df_site[1,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray25",
             label="Bridge",size=2,face="bold",nudge_y=-0.06)+
-  geom_point(data=df_site[2,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray24",size=1.5)+
-  geom_text(data=df_site[2,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray25",
+  geom_point(data=df_site[2,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray24",size=1.5)+
+  geom_text(data=df_site[2,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray25",
             label="Delta",size=2,face="bold",nudge_y=-0.06)+
-  geom_point(data=df_site[3,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray24",size=1.5)+
-  geom_text(data=df_site[3,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray25",
+  geom_point(data=df_site[3,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray24",size=1.5)+
+  geom_text(data=df_site[3,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray25",
             label="River.A",size=2,face="bold",nudge_y=-0.07)+
-  geom_point(data=df_site[4,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray24",size=1.5)+
-  geom_text(data=df_site[4,1:2,drop=FALSE],aes(x=RDA1,y=RDA2),color="gray25",
+  geom_point(data=df_site[4,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray24",size=1.5)+
+  geom_text(data=df_site[4,1:3,drop=FALSE],aes(x=PCoA1,y=PCoA2),color="gray25",
             label="River.B",size=2,face="bold",nudge_y=-0.07)+
   
   #Plot species abundance 
@@ -132,7 +132,7 @@ ggplot(df_species,aes(x=RDA1, y=RDA2))+
                      values=c(0,1,2,3,4,5,6,7,8,9))+  
   #each numbers in the shape values correspond to a specific shape
   
-#Additional customization to organize legends and spacing
+  #Additional customization to organize legends and spacing
   theme_classic()+
   theme(
     panel.spacing.x = unit(0.5,"cm"),
@@ -151,9 +151,8 @@ ggplot(df_species,aes(x=RDA1, y=RDA2))+
   #Axis Label and Scaling, adjustment depends on the output 
   labs(x=paste0("Component 1 (",RDA1_varex," %)"),
        y=paste0("Component 2 (",RDA2_varex," %)"))+
- 
-   
+  
+  
   scale_x_continuous(limits=c(-3,3)) +
   scale_y_continuous(limits=c(-2.5,2.5))
-
 
